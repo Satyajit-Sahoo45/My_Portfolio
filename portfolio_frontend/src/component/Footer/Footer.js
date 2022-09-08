c679b481075b5b44910ed57605a762469e07d6e0
Satyajit <sahoosatyajit2801@gmail.com>
2022-09-09 00:31:42 +0530

    portfolio

--- portfolio_frontend/src/component/Footer/Footer.js
+++ portfolio_frontend/src/component/Footer/Footer.js
@@ -9,6 +9,10 @@ const Footer = () => {
           <div className='img'>
             <h1> Portfolio </h1>
           </div>
+          <div>
+            <p> <i class="fa-sharp fa-solid fa-envelope"></i> sahoosatyajit2801@gmail.com</p>
+            <p> <i class="fa-solid fa-phone"></i> 7873585158</p>
+          </div>
           <p>© {new Date().getFullYear()}. All rights reserved. @Satyajit_Sahoo</p>
         </div>
       </footer>
